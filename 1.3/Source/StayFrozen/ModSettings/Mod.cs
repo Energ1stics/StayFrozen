0c3d76b1117c9ff51613f24de6f922e0a261b5db
Paul Hebst <paul.frank.hebst@gmail.com>
2021-09-12 16:30:53 +0200

    Implemented option for thermal balance duration.

---
+++ 1.3/Source/StayFrozen/ModSettings/Mod.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Verse;
+using SettingsHelper;
+using StayFrozen.Extensions;
+using StayFrozen.Utility;
+using System;
+
+namespace StayFrozen.ModSettings
+{
+    public class Mod : Verse.Mod
+    {
+        private Settings settings;
+
+        public Mod(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<Settings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listing = new Listing_Standard();
+            listing.Begin(inRect);
+            float timeToBalanceAsFloat = Settings.timeToBalance;
+            listing.AddLabeledSlider("HeatBalanceTimeLabel".Translate() + ": " + Utils.ToStringDuration(Settings.timeToBalance),
+                                     ref timeToBalanceAsFloat,
+                                     0, 120000);
+            Settings.timeToBalance = Convert.ToInt32(timeToBalanceAsFloat);
+            listing.End();
+            this.settings.Write();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "ItemTemperatureModName".Translate();
+        }
+    }
+}
---
+++ 1.3/Source/StayFrozen/ModSettings/Settings.cs
@@ -0,0 +1,15 @@
+﻿using Verse;
+
+namespace StayFrozen.ModSettings
+{
+    public class Settings : Verse.ModSettings
+    {
+        public static int timeToBalance = 40000;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref timeToBalance, nameof(timeToBalance), 40000);
+            base.ExposeData();
+        }
+    }
+}
